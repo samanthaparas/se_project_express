3b6090dba99b84458253ec1bbd35c379f37708ac
Samantha Paras <samantha.paras94@gmail.com>
2026-04-05 20:10:52 -0500

    Set up entry point, hot reload, mongoose connection, user schema, user routes and user controllers

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const mainRouter = require("./routes/index");
+
+const app = express();
+const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(console.error);
+
+app.use(express.json());
+app.use("/", mainRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ models/clothingItem.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+const clothingItemSchema = new mongoose.Schema({});
+
+module.exports = mongoose.model("item", clothingItemSchema);
