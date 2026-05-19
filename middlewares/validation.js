89496cecfcf4597021be53d2b10653ecd97e9b74
Samantha Paras <samantha.paras94@gmail.com>
2026-05-19 16:52:44 -0500

    Validate URLs for avatars and item images

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -12,7 +12,10 @@ const validateURL = (value, helpers) => {
 const validateCreateItem = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
-    imageUrl: Joi.string().required().custom(validateURL),
+    imageUrl: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "imageUrl" field must be filled in',
+      "string.uri": 'The "imageUrl" field must be a valid url',
+    }),
     weather: Joi.string().valid("hot", "warm", "cold").required(),
   }),
 });
@@ -20,7 +23,10 @@ const validateCreateItem = celebrate({
 const validateCreateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
-    avatar: Joi.string().required().custom(validateURL),
+    avatar: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "avatar" field must be filled in',
+      "string.uri": 'The "avatar" field must be a valid url',
+    }),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
