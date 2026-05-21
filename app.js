ebc5bea0986c38cb123955cb80eac49a863a4b42
Samantha Paras <samantha.paras94@gmail.com>
2026-05-20 19:40:39 -0500

    Install winston & create loggers

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 const { errors } = require("celebrate");
 const cors = require("cors");
 const express = require("express");
@@ -18,13 +19,16 @@ mongoose.connect("mongodb://localhost:27017/wtwr_db").catch(console.error);
 app.use(express.json());
 app.use(cors());
 
+app.use(requestLogger);
+
 app.post("/signin", validateLogin, login);
 app.post("/signup", validateCreateUser, createUser);
 
 app.use(routes);
 
-app.use(errors());
+app.use(errorLogger);
 
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT);
