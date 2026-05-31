49043988570e16427e80ce5191b63f1cb01e4e0f
Samantha Paras <samantha.paras94@gmail.com>
2026-05-31 10:42:14 -0500

    Add temporary crash test endpoint for code review

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.use(requestLogger);
 app.use(express.json());
 app.use(cors());
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.post("/signin", validateLogin, login);
 app.post("/signup", validateCreateUser, createUser);
 
